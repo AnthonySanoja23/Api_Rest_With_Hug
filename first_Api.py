581251992f6dce99f69659fe9ce95efaefa28ab0
AnthonySanoja23 <anthonysanoja95@gmail.com>
2019-09-01 12:50:51 -0400

    Llamada a api get

---
+++ first_Api.py
@@ -0,0 +1,25 @@
+import hug 
+
+
+
+Personas=[
+         {'Nombre':'Anthony',
+         'Apellido':'Sanoja',
+         'Edad':'23'},
+
+         {'Nombre':'Alejandro',
+         'Apellido':'Ortegano',
+         'Edad':'26'},
+
+         {'Nombre':'Cesar',
+         'Apellido':'Rivas',
+         'Edad':'24'},
+
+         ]
+
+
+@hug.get()
+
+def Get():
+    return Personas
+    
